77ba6e5acdec63a13a61b64527ca528f541912c1
Deepika Raghuraman <deeputharun@Deepikas-MacBook-Air.local>
2026-06-01 09:16:46 -0700

    adding locators

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -41,6 +41,12 @@ test("Title", function(){
 
 test("Title1", ()=>{
 
+    // Launch the url
+    // fill the username
+    // fill the password
+    // click on login button
+    // validate if the login is succ..
+
 })
 
 // async- await
@@ -50,3 +56,10 @@ test("Title1", ()=>{
 
 
 // JS & TS are asynchronous in nature
+
+
+// Locators 
+
+// 1. css selector - 2
+// 2. xpath - 3
+// 3. Playwright getBy locators - 1
